2464ef306a10cc16c0edaebf12dbf38ceef79a5b
liamhuo <liamhuo@tencent.com>
2021-07-13 15:41:57 +0800

    env vite配置

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # vite项目
 
-## 1. 项目初始化
+## 1. 安装使用
+
+```shell
+git clone https://github.com/huoyuhao/vue-ts.git
+npm install
+npm run dev
+# 打包
+npm run build
+# 修复前端规范
+npm run lint
+```
+
+## 2. 项目环境
+
+### 2.1 环境搭建
+
++ git项目创建
++ vite初始化
++ vite基础配置管理
++ vite环境变量ENV引入
++ vite打包配置
++ 测试环境请求代理、跨域配置
 
 ```shell
 npm init @vitejs/app admin
@@ -9,3 +30,4 @@ cd admin
 npm install
 npm run dev
 ```
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,58 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import type { UserConfig, ConfigEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import { loadEnv } from 'vite'
+import { wrapperEnv } from './build/utils';
+
+const path = require('path');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+  const viteEnv = wrapperEnv(env);
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, } = viteEnv;
+  const isBuild = command === 'build';
+
+  return {
+    base: VITE_PUBLIC_PATH,
+    publicDir: 'public',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      https: false,
+      cors: true,
+    },
+    esbuild: {
+      jsxFactory: 'h',
+      jsxFragment: 'Fragment',
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          // color - 全局默认主题颜色； width - 默认页面最小宽度
+          additionalData: '@color: #ef9103;@width: 1366px;',
+          javascriptEnabled: true,
+        },
+      },
+    },
+    build: {
+      target: 'es2015',
+      outDir: 'dist',
+      assetsDir: 'doppler',
+      assetsInlineLimit: 4096, // 4kb
+      // 默认为 Terser，虽然 Terser 相对较慢，但大多数情况下构建后的文件体积更小。ESbuild 最小化混淆更快但构建后的文件相对更大。
+      minify: isBuild ? 'terser' : 'esbuild',
+      // 启用/禁用 brotli 压缩大小报告。压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能
+      brotliSize: false,
+      // chunk 大小警告的限制（以 kbs 为单位）
+      chunkSizeWarningLimit: 2000,
+    },
+    plugins: [
+      vue(),
+    ]
+  }
+};
